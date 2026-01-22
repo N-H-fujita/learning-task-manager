3b477d38c2f0e3f732edd9bb970f04ca37b924b5
N-H-fujita <86991819+N-H-fujita@users.noreply.github.com>
2026-01-22 21:33:18 +0900

    feat: タスクを完了・未完了に切り替え可能にする

--- src/App.jsx
+++ src/App.jsx
@@ -8,15 +8,64 @@ function App() {
       completed: false,
     },
   ]);
+  const toggleCompleted = (id) => {
+    setTasks(
+      tasks.map((task) =>
+        task.id === id
+          ? { ...task, completed: !task.completed }
+          : task
+      )
+    );
+  };
+
+  const [title, setTitle] = useState('');
+  const handleAddTask = () => {
+    if(title === '') return;
+    setTasks([
+      ...tasks,
+      {
+        id: Date.now(),
+        title,
+        completed: false,
+      },
+    ]);
+    setTitle('');
+  };
 
   return (
     <main style={{ padding: '2rem' }}>
       <h1>Task Manager</h1>
+
+      <div>
+        <input
+          type="text"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <button onClick={handleAddTask}>追加</button>
+      </div>
+
       <ul>
         {tasks.map((task) => (
-          <li key={task.id}>{task.title}</li>
-        ))
-        }
+          <li key={task.id}>
+            <input
+              type='checkbox'
+              checked={task.completed}
+              onChange={() => toggleCompleted(task.id)}
+              style={{ cursor: 'pointer' }}
+            />
+            <span
+              onClick={() => toggleCompleted(task.id)}
+              style={{
+                cursor: 'pointer',
+                textDecoration: task.completed ? 'line-through' : 'none',
+                color: task.completed ? '#888' : '#000'
+              }}
+            >
+              {task.title}
+            </span>
+          </li>
+        ))}
       </ul>
     </main>
   )
